05dde7b7bb1947327dcc92f48fb9fa88b9b64604
DYM CANARIAS <dymcanarias@n2.local>
2018-08-09 10:05:03 +0100

    add init , girada, func contiene, protocolo ==

--- Domino/Ficha.swift
+++ Domino/Ficha.swift
@@ -28,22 +28,22 @@ struct Ficha: CustomStringConvertible, Equatable {
     
     // Inicialización
     init(puntos: ParejaPuntos) {
-        
+        self.puntos=puntos
     }
     
     // Devuelve la ficha actual girada
     var girada: Ficha {
-        
+       return Ficha(puntos: (izq:puntos.der,der:puntos.izq))
     }
     
     // Indica si alguna de las dos puntuaciones de entrada es igual a uno de los lado de la ficha
     func contiene(puntos: ParejaPuntos) -> Bool {
-        
+        return  puntos.izq == self.puntos.izq || puntos.der == self.puntos.der  || puntos.der == self.puntos.izq || puntos.izq == self.puntos.der
     }
     
     // Comparación de fichas
     static func == (lhs: Ficha, rhs: Ficha) -> Bool {
-        
+        return (lhs.puntos.izq == rhs.puntos.izq && lhs.puntos.der == rhs.puntos.der) || ( lhs.puntos.izq == rhs.puntos.der && lhs.puntos.der == rhs.puntos.izq)
     }
     
     var description: String {
